413d76909322aa60423461a0c862c156d1caa3d2
Zizhou Wang <hank2u@icloud.com>
2017-07-15 12:00:36 +0800

    Initial Commit

---
+++ Analog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Analog
+//
+//  Created by Zizhou Wang on 15/07/2017.
+//  Copyright © 2017 Zizhou Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
